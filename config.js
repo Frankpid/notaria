b1f7ae0a44a7e158bb06d8ee27fdf83dcf987fbf
Frank <fra.pinedo18@gmail.com>
2020-06-29 19:51:03 -0500

    29-06-2020-4

--- config.js
+++ config.js
@@ -1,7 +1,26 @@
 var dataConfig = {}
 const envm = process.env.NODE_ENV
 
-if( envm === "development" ){
+if( envm === "production" ){
+	dataConfig = {		
+		"URL": "https://142.93.3.176:3000",
+		"URL_BACK": "http://142.93.3.176:3001",
+		"PORT": 3000,
+		"NODE_ENV": "production",
+		"DB_HOST": "localhost",
+		"DB_USER": "devf",
+		"DB_PASSWORD": ")%w*)sZr$BRe",
+		"DB_NAME": "data_frbl",
+		"FB_APP_ID": "",
+		"FB_VERSION": 'v5.0',
+		"FB_APP_SECRET": "",
+		"GOOGLE_CLIENT_ID": "",
+		"GOOGLE_CLIENT_SECRET": "",
+		"SECURE": true,
+		"API_PATH": "http://142.93.3.176:3001/getapi"
+	}
+	console.log(dataConfig)
+}else if( envm === "development" ){	
 	dataConfig = {
 		"URL": "http://localhost:3000",
 		"URL_BACK": "http://localhost:4000",
@@ -18,25 +37,6 @@ if( envm === "development" ){
 		"GOOGLE_CLIENT_SECRET": "",
 		"SECURE": false,
 		"API_PATH": "http://localhost:4000/getapi"
-	}
-	console.log(dataConfig)
-}else{	
-	dataConfig = {
-		"URL": "https://142.93.3.176:3000",
-		"URL_BACK": "http://142.93.3.176:3001",
-		"PORT": 3000,
-		"NODE_ENV": "production",
-		"DB_HOST": "localhost",
-		"DB_USER": "devf",
-		"DB_PASSWORD": ")%w*)sZr$BRe",
-		"DB_NAME": "data_frbl",
-		"FB_APP_ID": "",
-		"FB_VERSION": 'v5.0',
-		"FB_APP_SECRET": "",
-		"GOOGLE_CLIENT_ID": "",
-		"GOOGLE_CLIENT_SECRET": "",
-		"SECURE": true,
-		"API_PATH": "http://142.93.3.176:3001/getapi"
 	}
 	console.log(dataConfig)
 }
