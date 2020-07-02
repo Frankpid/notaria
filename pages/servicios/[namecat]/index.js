1b44d83acc44cf9c17da525ff20067c15df40366
Frank <fra.pinedo18@gmail.com>
2020-07-02 13:02:31 -0500

    01-07-2020-12

--- pages/servicios/[namecat]/index.js
+++ pages/servicios/[namecat]/index.js
@@ -41,7 +41,7 @@ const ServicioCat = (props) => {
                         <div className="row slideshow-type-1">            
                             { 
                                 item.data.map((itemInner, index2) => {
-                                    return <ServiceListItem key={index2} data={itemInner} link={item.linkCat + '/' + itemInner.linkCat} class={'col col-md-3 col-sm-6'} />
+                                    return <ServiceListItem key={index2} data={itemInner} link={item.linkCat + '/' + itemInner.linkCat} class={'col col-md-3 col-sm-6 col-xs-12'} />
                                 })                        
                             }                                
                         </div>         
