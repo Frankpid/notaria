031760028518dd38617f588338b2603ee1fabf80
Frank <fra.pinedo18@gmail.com>
2020-07-28 14:11:22 -0500

    28-07-2020-change-2

--- config.js
+++ config.js
@@ -21,13 +21,13 @@ if( envm === "development" ){
 	}	
 }else{	
 	dataConfig = {
-		"URL": "http://142.93.3.176:3000",
-		"URL_BACK": "http://142.93.3.176:3001",
+		"URL": "http://164.90.224.40:3000",
+		"URL_BACK": "http://164.90.224.40:3001",
 		"PORT": 3000,
 		"NODE_ENV": "production",
 		"DB_HOST": "localhost",
-		"DB_USER": "devf",
-		"DB_PASSWORD": ")%w*)sZr$BRe",
+		"DB_USER": "",
+		"DB_PASSWORD": "",
 		"DB_NAME": "data_frbl",
 		"FB_APP_ID": "",
 		"FB_VERSION": 'v5.0',
@@ -35,7 +35,7 @@ if( envm === "development" ){
 		"GOOGLE_CLIENT_ID": "",
 		"GOOGLE_CLIENT_SECRET": "",
 		"SECURE": true,
-		"API_PATH": "http://142.93.3.176:3001/getapi"
+		"API_PATH": "http://164.90.224.40:3001/getapi"
 	}
 }
 
