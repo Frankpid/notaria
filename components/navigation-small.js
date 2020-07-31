1a6a745381d92ebaca0678875960af45597a2024
Frank <fra.pinedo18@gmail.com>
2020-07-30 22:39:29 -0500

    30-07-2020-change-2

--- components/navigation-small.js
+++ components/navigation-small.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Navigation = () =>{
     return (
         <div className="nav-small">
-            <a className="btn-reserve" href="http://www.notariaroman.pe/reservas/" target="_blank">Reserva de citas</a>                 
+            <a className="btn-reserve" href="http://reservas.notariaroman.pe" target="_blank">Reserva de citas</a>                 
             <a className="btn-fb" href="https://www.facebook.com/notariaroman/" target="_blank">Facebook</a>
         </div>
     )
